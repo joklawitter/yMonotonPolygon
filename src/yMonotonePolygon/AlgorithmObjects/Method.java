111d94c3fab13daf0681c66fbe9e3a5011d3acbe
Jo Klawitter <jo.klawitter@gmail.com>
2014-05-26 20:15:27 +0200

    methods hard coded as string arrays

--- src/yMonotonePolygon/AlgorithmObjects/Method.java
+++ src/yMonotonePolygon/AlgorithmObjects/Method.java
@@ -1,22 +1,60 @@
 package yMonotonePolygon.AlgorithmObjects;
 
 public enum Method {
-	HANLDE_START ("handleStartVertex"), 
-	HANLDE_END ("handleEndVertex"), 
-	HANLDE_SPLIT ("handleSplitVertex"), 
-	HANLDE_MERGE ("handleMergeVertex"), 
-	HANLDE_REGULAR ("handleRegularVertex");
+	HANLDE_START ("handleStartVertex(v)",
+			new String[] 
+					{"insert v.nextEdge in T",
+					"set v.nextEdge.helper = v"}), 
+	HANLDE_END ("handleEndVertex(v)",
+			new String[] 
+					{"if v.prevEdge.helper is merge vertex",
+					"  then insert diagonal v-v.prevEdge.helper",
+					"delete v.prevEdge from T"}), 
+	HANLDE_SPLIT ("handleSplitVertex(v)",
+			new String[] 
+					{"search in T to find edge e directly left of v",
+					"insert diagonal v-e.helper",
+					"set e.helper = v",
+					"insert v.nextEdge in T",
+					"set v.nextEdge.helper = v"}), 
+	HANLDE_MERGE ("handleMergeVertex(v)",
+			new String[] 
+					{"if v.prevEdge.helper is merge vertex",
+					"  then insert diagonal v-v.prevEdge.helper",
+					"delete v.prevEdge from T",
+					"search in T to find edge e directly left of v",
+					"if e.helper is merge vertex",
+					"  then insert diagonal v-e.helper",
+					"set e.helper = v"}), 
+	HANLDE_REGULAR ("handleRegularVertex(v)",
+			new String[] 
+					{"if interior of P lies to the right of v",
+					"  then if v.prevEdge.helper is merge vertex",
+					"         then insert diagonal v-v.prevEdge.helper",
+					"  delete v.prevEdge from T",
+					"  insert v.nextEdge in T",
+					"  set v.nextEdge.helper = v",
+					"else search in T to find edge e directly left of v",
+					"  if e.helper is merge vertex",
+					"     then insert diagonal v-e.helper",
+					"  set e.helper = v"});
 	
 	private final String name;
-	//private String[] lines;
-	// TODO
+	private final String[] lines;
 	
-	private Method(final String name) {
+	private Method(final String name, final String[] lines) {
 		this.name = name;
-		//this.lines = lines;
+		this.lines = lines;
 	}
 
 	public String getName() {
 		return name;
 	}
+
+	
+	public String[] getLines() {
+		return lines;
+	}
+
+
 }
